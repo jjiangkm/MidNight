5741ff5535260993c40b5f9f88f23e29b918ba33
蒋坤明 <kunming.jiang@bizfocus.cn>
2017-07-25 17:46:45 +0800

    android左右滑动切页

--- app/App.js
+++ app/App.js
@@ -5,6 +5,7 @@ import { StackNavigator } from 'react-navigation';
 import HistoryToday from './historytoday/HistoryToday'
 import HistoryTodayDetail from './historytoday/HistoryTodayDetail'
 import ImageView from './component/ImageView'
+import CardStackStyleInterpolator from 'react-navigation/src/views/CardStackStyleInterpolator';//android左右滑动切页
 const SimpleApp = StackNavigator({
     HistoryToday: { screen: HistoryToday },
     HistoryTodayDetail: { screen: HistoryTodayDetail },
@@ -16,6 +17,9 @@ const SimpleApp = StackNavigator({
         headerStyle: { backgroundColor: '#039069'},
         gesturesEnabled:true
     },
+    transitionConfig:()=>({
+        screenInterpolator: CardStackStyleInterpolator.forHorizontal,
+    }),
     mode: 'card',  // 页面切换模式, 左右是card(相当于iOS中的push效果), 上下是modal(相当于iOS中的modal效果)
     headerMode: 'screen', // 导航栏的显示模式, screen: 有渐变透明效果, float: 无透明效果, none: 隐藏导航栏
     onTransitionStart: ()=>{ console.log('导航栏切换开始'); },  // 回调
